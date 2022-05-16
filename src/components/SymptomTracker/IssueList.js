ed1891d8b26bea1e3f93146bf9107f95999fc3a6
Caleb Smith <minifiedcode@gmail.com>
2022-05-15 23:21:48 -0500

    Add better visibility to each issue

--- src/components/SymptomTracker/IssueList.js
+++ src/components/SymptomTracker/IssueList.js
@@ -1,13 +1,21 @@
+import Button from 'react-bootstrap/Button'
 import { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function IssueList({ issues }) {
     return (
-        <div>
+        <div className="issue-list">
             {issues.length > 0 &&
                 issues.map(issue => {
                     return (
-                        <NavLink to={`/user/1/issue/${issue.name}`} key={issue.name}>{issue.name}</NavLink>
+                        <div className="d-grid gap-2">
+                            <Button className="mt-3" variant="outline-primary">
+                                <Link to={`/user/1/issue/${issue.name}`} key={issue.name}>
+                                    {issue.name}
+                                </Link>
+                            </Button>
+
+                        </div>
                     )
                 })
             }
